75be758d9293683acbc69bc60716e7ccb89048d9
vitrum.zhu <vitrum.cn@gmail.com>
2013-10-16 23:08:19 +0800

    update

--- www/script/mt2.js
+++ www/script/mt2.js
@@ -336,6 +336,8 @@ $(document).ready(function(){
       if(num >0 ){
         console.log('ClimerTime:' + num + '\n');
         console.log(fntA.ClimerOn);
+        $('.light span').removeClass().addClass('lite'+num);
+        $('.gamenote span').removeClass().addClass('note'+num);
       }
       if(num === 0) {
         if(!fntA.startime){
@@ -382,8 +384,9 @@ $(document).ready(function(){
               console.log('You lost!');
             }
           }else{
-            console.log('Your time is out.')
+            console.log('Your time is out.');
           }
+          console.log('stop the Animation.');
           stopAnimation();
           break;
       }
@@ -392,10 +395,10 @@ $(document).ready(function(){
     function drawRectangle(myRectangle, context) {
       context.beginPath();
       context.rect(myRectangle.x, myRectangle.y, myRectangle.width, myRectangle.height);
-      context.fillStyle = 'green';
+      context.fillStyle = 'blue';
       context.fill();
       context.lineWidth = myRectangle.borderWidth;
-      context.strokeStyle = 'green';
+      context.strokeStyle = 'blue';
       context.stroke();
     }
     function animate() {
@@ -421,7 +424,7 @@ $(document).ready(function(){
 
     drawRectangle(myRectangle, context);
     var startTime = (new Date()).getTime();
-    animate(myRectangle, canvas, context, startTime)
+    // animate(myRectangle, canvas, context, startTime);
     function stopAnimation(e) {
         // use the requestID to cancel the requestAnimationFrame call
         cancelRAF(fntA.requestId);
@@ -449,315 +452,7 @@ $(document).ready(function(){
   }//climer game
 
   //main run
-  function fntRun(){
-    fntA.requestId = 0;
-    fntA.startime = 0;
-    fntA.image0 = new Image();  
-    fntA.path0 = new Image();  
-    fntA.imageMini = new Image();
-    fntA.w = 320;  
-    fntA.h = 491; 
-    var ctx0,ctx1;  
-    var y0,y1,y2,y3,y4,y5;  
-    //fntA.gameLevel = 1;
-    fntA.allmoveA = 0;
-    fntA.allmoveB = 0;
-    fntA.alltimes = 0;
-    fntA.mapitem = 40;
 
-    $('.iframbox iframe').remove();
-
-    funMapload();
-    function countdownNewTime(secs) {
-      //countdown
-      secs = Number(secs);
-      for (var i = secs; i >= 0; i--) {
-        (function(index) {
-          setTimeout(function(){
-          doUpdateTime(index);
-        }, (secs - index) * 1000);
-      })(i);
-      }
-    }
-    function doUpdateTime(num) {
-      //console.log('now countdown number is :' + num);
-      if(num == 9 || num == 8){
-        showSubMask('gamemask','howplay');
-        $('.gamemask .countdown').html('');
-      }
-      if(num == 6 || num == 7 || num == 5 ){
-        showSubMask('gamemask','connection');
-      }
-      if(num == 4 || num == 3 || num == 2 ){
-        showSubMask('gamemask','countdown');
-        $('.gamemask .countdown').html('<span class="'+ (num-1) + '">' + (num-1) + '</span>');
-      }
-      if(num === 1) {
-        showSubMask('gamemask','countdown');
-        $('.gamemask .countdown').html('<div class="pao"></div>');
-      }
-      if(num === 0) {
-        if(!fntA.startime){
-          showSubMask('gamemask');
-          $('.gamemask .countdown').html();
-          clearTimeout();
-          start();
-        }
-      }
-    }
-    function countdownGameTime(secs) {
-      //countdown
-      secs = Number(secs);
-      for (var i = secs; i >= 0; i--) {
-        (function(index) {
-          setTimeout(function(){
-          doUpdateGameTime(index);
-        }, (secs - index) * 1000);
-      })(i);
-      }
-    }
-    function doUpdateGameTime(num) {
-      $('.gametime .countdown').html('<span class="'+ num + '">' + num + '</span>');
-      if(num === 0) {
-        if(!fntA.gameFinish){
-          fntA.gameFinish = true;
-        }
-      }
-    }
-    function runInit() {
-      ctx0 =  document.getElementById('canvas').getContext('2d');
-      fntA.mapArr = new Array();
-      fntA.mapPathArr = new Array();
-      y0 = 0;
-      y1 = -1*fntA.h;
-      y2 = -2*fntA.h;
-      y3 = 0;
-      y4 = -1*fntA.h;
-      y5 = -2*fntA.h;
-      
-      //the hard game level
-      var defLevel = fRandomBy(1,100);
-      if(defLevel>60){
-        fntA.gameLevel = 2;
-      }else{
-        fntA.gameLevel = 1;
-      }
-      //the hard game level
-
-        //generat map
-        if(fntA.gameLevel != 3){
-          for (var i = fntA.mapitem - 1; i >= 0; i--) {
-            if (i === 20){
-              fntA.mapArr.push(fntA.imgArr[9]);
-              fntA.mapPathArr.push(fntA.pathArr[9]);
-            }else if (i === 10){
-              fntA.mapArr.push(fntA.imgArr[10]);
-              fntA.mapPathArr.push(fntA.pathArr[10]);
-            }else if (i === 0){
-              fntA.mapArr.push(fntA.imgArr[11]);
-              fntA.mapPathArr.push(fntA.pathArr[11]);
-            }
-          }
-          //  console.log(fntA.mapArr);
-        }
-        //set map image
-        fntA.image0.src = fntA.mapArr[0];  
-        fntA.imageMini.src = fntA.imgArr[12];
-
-
-
-      setTimeout(function () {
-        ctx0.drawImage(fntA.image0,0,y0,fntA.w,fntA.h);  
-        //  console.log('draw default map image');
-      }, 40);
-
-
-    }
-    //}
-    runInit();
-      function start() {
-        
-        fntA.moveA = 1;
-        fntA.moveB = 20;
-        fntA.allmoveA = 0;
-        fntA.allmoveB = 80;
-        fntA.alltimes = 0;
-        fntA.alltimesB = 0;
-        fntA.shakeEng = 0;
-        countdownGameTime(20);
-
-        $('.player').addClass('running');
-        //console.log("start");
-        // if (window.performance.now) {
-        //     startime = window.performance.now();
-        // } else {
-          fntA.startime = Date.now();
-        // }
-        fntA.requestId = window.requestAFrame(render);
-      }
-      function stop() {
-        if (fntA.requestId)
-          window.cancelAFrame(fntA.requestId); 
-        $('.player').removeClass('running'); 
-        //clear
-        //ctx0.clearRect(0,0,fntA.w,fntA.h);
-        //ctx1.clearRect(0,0,fntA.w,fntA.h); 
-        ctxMini.clearRect(0,0,fntA.w,fntA.h);     
-      }
-      function wayRoll(e) {
-        if((e+5) > fntA.mapitem ){
-          e = (e+5) % fntA.mapitem;
-        }
-        return e;
-      }
-      function render(time) {
-        //clear
-        ctx0.clearRect(0,0,fntA.w,fntA.h);
-
-        //draw now
-        var move = Math.floor(fntA.moveA);
-        var moveB = Math.floor(fntA.moveB);
-        //ctx a
-        y0 +=move;  
-        y1 +=move;  
-        y2 +=move; 
-        //console.log("new: y0=" + y0 + ",y1=" + y1 + ",y2=" + y2 + ",move=" + move + ",fntA.moveA=" + fntA.moveA); 
-        if(y0>=fntA.h){  
-          //y0=move-2*fntA.h;  
-          y0 = Math.min(y1,y2) - fntA.h;
-          fntA.alltimes++;
-          fntA.image0.src = fntA.mapArr[wayRoll((Number(fntA.alltimes)+3))]; 
-          fntA.path0.src = fntA.mapPathArr[wayRoll((Number(fntA.alltimes)+3))]; 
-          //  console.log("y0 new image:" + fntA.image0.src + "fntA.alltimes:"+fntA.alltimes);
-        }  
-        //draw now
-        ctx0.drawImage(fntA.image0,0,y0,fntA.w,fntA.h);  
-        //set requestId
-        fntA.requestId = window.requestAFrame(render);
-        //set stop process
-        if(fntA.shakeEng < 6){
-          fntA.moveA = fntA.moveA * 0.977;
-          if (fntA.moveA<0.8) {fntA.moveA=0.8};
-        }else{
-          if (fntA.moveA<20) {fntA.moveA = fntA.moveA * 1.12;}
-          if (fntA.moveA>20 && fntA.moveA<50) {fntA.moveA = fntA.moveA * 1.13;}
-          if (fntA.moveA>=50) {fntA.moveA = fntA.moveA * 1.14;}
-        }
-        fntA.moveA = Math.max(0.8, Math.min(12, fntA.moveA));
-        //console.log("old: y0=" + y0 + ",y1=" + y1 + ",y2=" + y2 + ",move=" + move + ",fntA.alltimes=" + fntA.alltimes);
-        //if(fntA.moveA<=4){
-        fntA.allmoveA +=move; 
-        fntA.allmoveB +=moveB; 
-        fntA.shakeEng = fntA.shakeEng -1;
-        if (fntA.shakeEng<1) { fntA.shakeEng = 1};
-        if (fntA.allmoveA===fntA.allmoveB){fntA.allmoveA=fntA.allmoveA+1;}
-        $(".playerinfoa .playrecord").html(fntA.allmoveA + '米');
-        $(".playerinfob .playrecord").html(fntA.allmoveB + '米');
-        // console.log('fntA.shakeEng:' + fntA.shakeEng + ',fntA.moveA:' + fntA.moveA + ',fntA.allmoveA:' + fntA.allmoveA + ',fntA.gameLevel:' + fntA.gameLevel);
-
-        //game resort
-        if( fntA.gameFinish ){
-          //  console.log("stop running at " + time + ", and allmoveA = " + fntA.allmoveA + ",fntA.alltimes= " +fntA.alltimes);
-          stop();
-          showSubMask('gamemask','loading');
-          if(fntA.allmoveA>=fntA.allmoveB){ 
-            fntA.gameResult = 'win';
-          }else{
-            fntA.gameResult = 'lost';
-          }
-          //id,name,record,result
-          postGameRecord(fntA.playerId,fntA.playerName,fntA.allmoveA,fntA.gameResult);
-          fntA.gameFinish = true;
-        }
-        //Game AI
-        if(fntA.gameLevel ===1 ){
-          if (fntA.allmoveB < 2000) {
-            if( (fntA.allmoveA - fntA.allmoveB) >200 ){
-              fntA.moveB = fntA.moveB * 1.05;
-              fntA.moveB = Math.min( 16, Math.max(12,fntA.moveB));
-            }
-            if( (fntA.allmoveB - fntA.allmoveA) >100 ){
-              fntA.moveB = fntA.moveB * 0.9992;
-              fntA.moveB = Math.min( 18, Math.max(5,fntA.moveB));
-            }
-          }
-          
-          
-        }
-        // var tmpPlayerTopa , tmpPlayerTopb;
-        // tmpPlayerTopa = 320 - fntA.moveA*8;
-        // tmpPlayerTopb = 320 - fntA.moveB*10;
-        // $('.playera').css('top',tmpPlayerTopa+'px');
-        // $('.playerb').css('top',tmpPlayerTopb+'px');
-        //the hard game level
-        //mini map
-
-
-      }
-      // handle multiple browsers for requestAnimationFrame()
-      //runInit();
-      $('.startrun').on('click',function(){
-        start();
-      });
-      $('.stop').on('click',function(){
-        stop();
-      });
-    // NodeJS Server
-    var nodejs_server = "222.73.241.60:8082";
-    // connect
-    var socket = io.connect("http://" + nodejs_server);
-
-    socket.emit("send", {
-        key: fntA.key,
-        act: "pcenter"
-    });
-
-
-
-    socket.on("get_response", function (b) {
-      var combine = b.key + "_" + b.act;
-      // console.log(combine);
-      switch (combine) {
-        // when open m.page，call enter event，then show the game
-        case fntA.key + "_enter":
-          setTimeout(function () {
-            if(fntA.allmoveB==0){
-              showSubFrame('runbox','rundivbox');
-              
-              if(!fntA.gameOn){
-                
-                fntA.gameOn = true;
-                countdownNewTime(9);
-              }
-            }
-          }, 500);
-          break;
-        // shake event
-        case fntA.key + "_changebg":
-          //console.log('fntA.gameOn:' + fntA.gameOn +'fntA.shakerecord:' + fntA.shakerecord +'fntA.gameFinish:' + fntA.gameFinish );
-
-          // if(fntA.gameOn && fntA.shakerecord === 1 && !fntA.gameFinish){
-
-          //   //console.log('call 1 countdown');
-          //   countdownNewTime(6);
-          // }
-          shakeEventDidOccur();
-          if(fntA.shakeEng<3 && fntA.moveA <1){
-            fntA.moveA = 2;
-          }
-          if(fntA.shakeEng<30){
-            fntA.shakeEng = fntA.shakeEng + 6 ;
-          }
-
-          break;
-      }
-    });
-
-  }
-  function shakeEventDidOccur() {
-    //put your own code here etc.
-    fntA.shakerecord = fntA.shakerecord + 1;
-  }
   var pageUrl = 'http://www.quyeba.com/event/explorerchallenge/m_c.html'; 
 
   //fntA.gameLevel = 1;
